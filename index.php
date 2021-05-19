159e4a0881e70426cb6a9834d736f2e884e68ded
Nolan Resu <nolan.resu@yandex.com>
2021-05-19 16:04:17 +0800

    added foundation framework

---
+++ index.php
@@ -0,0 +1,264 @@
+
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Foundation for Sites</title>
+  <link rel="stylesheet" href="assets/css/foundation.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/my-css.css">
+</head>
+<body>
+
+  <div data-sticky-container class="sticky-container" style="height: 72px;">
+    <div class="title-bar sticky is-anchored is-at-top" data-sticky="wwimnd-sticky" data-options="marginTop:0;" data-resize="cm0shu-sticky" data-mutate="cm0shu-sticky" data-events="mutate" style="max-width: 533px; margin-top: 0px; bottom: auto; top: 0px;">
+      <div class="title-bar-left">
+        <span class="title-bar-title">Title here</span>
+      </div>
+      <div class="title-bar-right">
+        <ul class="dropdown menu align-right" data-dropdown-menu="fd3ebc-dropdown-menu" role="menubar">
+          <li role="none" class="is-dropdown-submenu-parent opens-left" aria-haspopup="true" aria-label="Item 1" data-is-click="false">
+            <a role="menuitem">Item 1</a>
+            <ul class="menu submenu is-dropdown-submenu first-sub vertical" data-submenu="" role="menubar" style="">
+              <li role="none" class="is-submenu-item is-dropdown-submenu-item"><a href="#" role="menuitem">Item 1A</a></li>
+              <li role="none" class="is-dropdown-submenu-parent is-submenu-item is-dropdown-submenu-item opens-left" aria-haspopup="true" aria-label="Item 1B">
+                <a href="#" role="menuitem">Item 1B</a>
+                <ul class="menu submenu is-dropdown-submenu vertical" data-submenu="" role="menubar">
+                  <li role="none" class="is-submenu-item is-dropdown-submenu-item"><a href="#" role="menuitem">Item 1B i</a></li>
+                  <li role="none" class="is-submenu-item is-dropdown-submenu-item"><a href="#" role="menuitem">Item 1B ii</a></li>
+                  <li role="none" class="is-dropdown-submenu-parent is-submenu-item is-dropdown-submenu-item opens-left" aria-haspopup="true" aria-label="Item 1B iii">
+                    <a href="#" role="menuitem">Item 1B iii</a>
+                    <ul class="menu submenu is-dropdown-submenu vertical" data-submenu="" role="menubar">
+                      <li role="none" class="is-submenu-item is-dropdown-submenu-item"><a href="#" role="menuitem">Item 1B iii alpha</a></li>
+                      <li role="none" class="is-submenu-item is-dropdown-submenu-item"><a href="#" role="menuitem">Item 1B iii omega</a></li>
+                    </ul>
+                  </li>
+                  <li role="none" class="is-dropdown-submenu-parent is-submenu-item is-dropdown-submenu-item opens-left" aria-haspopup="true" aria-label="Item 1B iv">
+                    <a href="#" role="menuitem">Item 1B iv</a>
+                    <ul class="menu submenu is-dropdown-submenu vertical" data-submenu="" role="menubar">
+                      <li role="none" class="is-submenu-item is-dropdown-submenu-item"><a href="#" role="menuitem">Item 1B iv alpha</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+              <li role="none" class="is-submenu-item is-dropdown-submenu-item"><a href="#" role="menuitem">Item 1C</a></li>
+            </ul>
+          </li>
+          <li role="none" class="is-dropdown-submenu-parent opens-left" aria-haspopup="true" aria-label="Item 2" data-is-click="false">
+            <a href="#" role="menuitem">Item 2</a>
+            <ul class="menu submenu is-dropdown-submenu first-sub vertical" data-submenu="" role="menubar" style="">
+              <li role="none" class="is-submenu-item is-dropdown-submenu-item"><a href="#" role="menuitem">Item 2A</a></li>
+              <li role="none" class="is-submenu-item is-dropdown-submenu-item"><a href="#" role="menuitem">Item 2B</a></li>
+            </ul>
+          </li>
+          <li role="none"><a href="#" role="menuitem">Item 3</a></li>
+          <li role="none"><a href="#" role="menuitem">Item 4</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <!--<div class="row">
+    <div class="columns">
+      <ul class="vertical menu" data-responsive-menu="drilldown medium-accordion" style="max-width: 250px;">
+        <li>
+          <a href="#">Item 1</a>
+          <ul class="vertical menu">
+            <li>
+              <a href="#">Item 1A</a>
+              <ul class="vertical menu">
+                <li><a href="#">Item 1A</a></li>
+                <li><a href="#">Item 1B</a></li>
+                <li><a href="#">Item 1C</a></li>
+                <li><a href="#">Item 1D</a></li>
+                <li><a href="#">Item 1E</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Item 1B</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Item 2</a>
+          <ul class="vertical menu">
+            <li><a href="#">Item 2A</a></li>
+            <li><a href="#">Item 2B</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Item 3</a>
+          <ul class="vertical menu">
+            <li><a href="#">Item 3A</a></li>
+            <li><a href="#">Item 3B</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div class="columns">
+      <div class="grid-x grid-padding-x">
+        <div class="large-12 cell">
+          <h1>Welcome</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+   <div class="grid-container"> 
+    <div class="grid-x grid-padding-x">
+      <div class="large-12 cell">
+        <h1>Welcome to Foundation</h1>
+      </div>
+    </div>
+
+    <div class="grid-x grid-padding-x">
+      <div class="large-12 cell">
+        <div class="callout">
+          <h3>We&rsquo;re stoked you want to try Foundation! </h3>
+          <p>To get going, this file (index.html) includes some basic styles you can modify, play around with, or totally destroy to get going.</p>
+          <p>Once you've exhausted the fun in this document, you should check out:</p>
+          <div class="grid-x grid-padding-x">
+            <div class="large-4 medium-4 cell">
+              <p><a href="http://foundation.zurb.com/docs">Foundation Documentation</a><br />Everything you need to know about using the framework.</p>
+            </div>
+            <div class="large-4 medium-4 cell">
+              <p><a href="http://zurb.com/university/code-skills">Foundation Code Skills</a><br />These online courses offer you a chance to better understand how Foundation works and how you can master it to create awesome projects.</p>
+            </div>
+            <div class="large-4 medium-4 cell">
+              <p><a href="http://foundation.zurb.com/forum">Foundation Forum</a><br />Join the Foundation community to ask a question or show off your knowlege.</p>
+            </div>
+          </div>
+          <div class="grid-x grid-padding-x">
+            <div class="large-4 medium-4 medium-push-2 cell">
+              <p><a href="http://github.com/zurb/foundation">Foundation on Github</a><br />Latest code, issue reports, feature requests and more.</p>
+            </div>
+            <div class="large-4 medium-4 medium-pull-2 cell">
+              <p><a href="https://twitter.com/ZURBfoundation">@zurbfoundation</a><br />Ping us on Twitter if you have questions. When you build something with this we'd love to see it (and send you a totally boss sticker).</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+     <div class="grid-x grid-padding-x">
+      <div class="large-8 medium-8 cell">
+        <h5>Here&rsquo;s your basic grid:</h5>
+        <!-- Grid Example --
+
+        <div class="grid-x grid-padding-x">
+          <div class="large-12 cell">
+            <div class="primary callout">
+              <p><strong>This is a twelve cell section in a grid-x.</strong> Each of these includes a div.callout element so you can see where the cell are - it's not required at all for the grid.</p>
+            </div>
+          </div>
+        </div>
+        <div class="grid-x grid-padding-x">
+          <div class="large-6 medium-6 cell">
+            <div class="primary callout">
+              <p>Six cell</p>
+            </div>
+          </div>
+          <div class="large-6 medium-6 cell">
+            <div class="primary callout">
+              <p>Six cell</p>
+            </div>
+          </div>
+        </div>
+        <div class="grid-x grid-padding-x">
+          <div class="large-4 medium-4 small-4 cell">
+            <div class="primary callout">
+              <p>Four cell</p>
+            </div>
+          </div>
+          <div class="large-4 medium-4 small-4 cell">
+            <div class="primary callout">
+              <p>Four cell</p>
+            </div>
+          </div>
+          <div class="large-4 medium-4 small-4 cell">
+            <div class="primary callout">
+              <p>Four cell</p>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <h5>We bet you&rsquo;ll need a form somewhere:</h5>
+        <form>
+          <div class="grid-x grid-padding-x">
+            <div class="large-12 cell">
+              <label>Input Label</label>
+              <input type="text" placeholder="large-12.cell" />
+            </div>
+          </div>
+          <div class="grid-x grid-padding-x">
+            <div class="large-4 medium-4 cell">
+              <label>Input Label</label>
+              <input type="text" placeholder="large-4.cell" />
+            </div>
+            <div class="large-4 medium-4 cell">
+              <label>Input Label</label>
+              <input type="text" placeholder="large-4.cell" />
+            </div>
+            <div class="large-4 medium-4 cell">
+              <div class="grid-x">
+                <label>Input Label</label>
+                <div class="input-group">
+                  <input type="text" placeholder="small-9.cell" class="input-group-field" />
+                  <span class="input-group-label">.com</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="grid-x grid-padding-x">
+            <div class="large-12 cell">
+              <label>Select Box</label>
+              <select>
+                <option value="husker">Husker</option>
+                <option value="starbuck">Starbuck</option>
+                <option value="hotdog">Hot Dog</option>
+                <option value="apollo">Apollo</option>
+              </select>
+            </div>
+          </div>
+          <div class="grid-x grid-padding-x">
+            <div class="large-6 medium-6 cell">
+              <label>Choose Your Favorite</label>
+              <input type="radio" name="pokemon" value="Red" id="pokemonRed"><label for="pokemonRed">Radio 1</label>
+              <input type="radio" name="pokemon" value="Blue" id="pokemonBlue"><label for="pokemonBlue">Radio 2</label>
+            </div>
+            <div class="large-6 medium-6 cell">
+              <label>Check these out</label>
+              <input id="checkbox1" type="checkbox"><label for="checkbox1">Checkbox 1</label>
+              <input id="checkbox2" type="checkbox"><label for="checkbox2">Checkbox 2</label>
+            </div>
+          </div>
+          <div class="grid-x grid-padding-x">
+            <div class="large-12 cell">
+              <label>Textarea Label</label>
+              <textarea placeholder="small-12.cell"></textarea>
+            </div>
+          </div>
+        </form>
+      </div>
+
+      <div class="large-4 medium-4 cell">
+        <h5>Try one of these buttons:</h5>
+        <p><a href="#" class="button">Simple Button</a><br/>
+          <a href="#" class="success button">Success Btn</a><br/>
+          <a href="#" class="alert button">Alert Btn</a><br/>
+          <a href="#" class="secondary button">Secondary Btn</a></p>
+          <div class="callout">
+            <h5>So many components, girl!</h5>
+            <p>A whole kitchen sink of goodies comes with Foundation. Check out the docs to see them all, along with details on making them your own.</p>
+            <a href="https://foundation.zurb.com/sites/docs/" class="small button">Go to Foundation Docs</a>
+          </div>
+        </div>
+      </div>
+    </div>-->
+
+
+    <script src="assets/js/vendor.js"></script>
+    <script src="assets/js/foundation.js"></script>
+    <script src="assets/js/my-js.js"></script>
+  </body>
+  </html>
