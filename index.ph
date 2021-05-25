e47158634ef4cbf59d24655f75002be9ec77a92c
Nolan Resu <nolan.resu@yandex.com>
2021-05-26 06:34:15 +0800

    updates

---
+++ index.ph
@@ -0,0 +1,106 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>PSA-IMS | Welcome</title>
+  
+  <!-- Compressed CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css" integrity="sha256-ogmFxjqiTMnZhxCqVmcqTvjfe1Y/ec4WaRj/aQPvn+I=" crossorigin="anonymous">
+
+  <link rel="stylesheet" type="text/css" href="assets/css/my-css.css">
+
+</head>
+<body>
+
+  <nav class="top-bar topbar-responsive">
+    <div class="top-bar-title">
+      <span data-responsive-toggle="topbar-responsive" data-hide-for="medium">
+        <button class="menu-icon" type="button" data-toggle></button>
+      </span>
+      <a class="topbar-responsive-logo" href="#"><strong>Site Title</strong></a>
+    </div>
+    <div id="topbar-responsive" class="topbar-responsive-links">
+      <div class="top-bar-right">
+        <ul class="menu simple vertical medium-horizontal">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Works</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Contact</a></li>
+          <li>
+            <button type="button" class="button hollow topbar-responsive-button">Categories</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+  <script src="assets/js/vendor.js"></script>
+  <script src="assets/js/foundation.js"></script>
+  <script src="assets/js/my-js.js"></script>
+  <script>
+    $(document).foundation();
+  </script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,106 +1,74 @@
 <!doctype html>
-<html class="no-js" lang="en">
+<html lang="en" id="top">
 <head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>PSA-IMS | Welcome</title>
-  
-  <!-- Compressed CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css" integrity="sha256-ogmFxjqiTMnZhxCqVmcqTvjfe1Y/ec4WaRj/aQPvn+I=" crossorigin="anonymous">
+  <meta charset="UTF-8">
+
+  <link href="assets/css/main.min.css" rel="stylesheet">
+  <link href="assets/css/app.min.css" rel="stylesheet">
 
   <link rel="stylesheet" type="text/css" href="assets/css/my-css.css">
 
+  <title>PSA IMS | Inventory Management System</title>
 </head>
+
 <body>
+  <nav class="tab-bar">
+    <section class="left-small">
+      <a class="menu-icon" id="menu-toggle"><span></span></a>
+    </section>
 
-  <nav class="top-bar topbar-responsive">
-    <div class="top-bar-title">
-      <span data-responsive-toggle="topbar-responsive" data-hide-for="medium">
-        <button class="menu-icon" type="button" data-toggle></button>
-      </span>
-      <a class="topbar-responsive-logo" href="#"><strong>Site Title</strong></a>
-    </div>
-    <div id="topbar-responsive" class="topbar-responsive-links">
-      <div class="top-bar-right">
-        <ul class="menu simple vertical medium-horizontal">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Services</a></li>
-          <li><a href="#">Works</a></li>
-          <li><a href="#">News</a></li>
-          <li><a href="#">Contact</a></li>
-          <li>
-            <button type="button" class="button hollow topbar-responsive-button">Categories</button>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <section class="middle tab-bar-section">
+      Inventory Management System
+    </section>
   </nav>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
 
-  <script src="assets/js/vendor.js"></script>
-  <script src="assets/js/foundation.js"></script>
-  <script src="assets/js/my-js.js"></script>
-  <script>
-    $(document).foundation();
-  </script>
-</body>
-</html>
+  <div id="wrapper" class="toggled">
+    <!-- side bar -->
+    <div id="sidebar-wrapper" class="toggled">
+      <ul class="sidebar-nav toggled">
+        <li class="sidebar-brand">
+          <a href="#">
+            Admin
+          </a>
+        </li>
+        <li>
+          <a href="#" alt="Dashboard">Dashboard<span class="fi-graph-pie" alt="Dashboard"></a>
+          </li>
+          <li>
+            <a href="#" alt="Shortcuts">Shortcuts<span class="fi-link" alt="Shortcuts"></a>
+            </li>
+            <li>
+              <a href="#">Overview<span class="fi-results" alt="Overview"></a>
+              </li>
+              <li>
+                <a href="#">About<span class="fi-star" alt="About"></a>
+                </li>
+                <li>
+                  <a href="#">Contact<span class="fi-address-book" alt="Contact"></a>
+                  </li>
+                </ul>
+              </div>
+
+              <!-- page content -->
+              <div id="page-content-wrapper" class="toggled">
+                <div class="">
+                  <div class="row">
+                    <div class="large-12 columns">
+                      <h1>Dashboard</h1>
+                      <br><br><br><br><br><br><br><br><br><br><br><br>
+                      <br><br><br><br><br><br><br><br><br><br><br><br>
+                      <br><br><br><br><br><br><br><br><br><br><br><br>
+                      <br><br><br><br><br><br><br><br><br><br><br><br>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+
+
+            <script src="assets/js/main.min.js"></script>
+            <script src="assets/js/app.min.js"></script>
+
+          </body>
+          </html>
